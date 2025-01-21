aec02d39375fa4857a634bf2f74b24c17531e2f6
Tomaicho <tomas.a.lima@gmail.com>
2025-01-21 12:05:45 +0100

    fix

--- methods.py
+++ methods.py
@@ -44,11 +44,12 @@ def method_II_segment(t1_image, t2_image):
     print("\nParameters file modified to be applicable for labels.\n")
 
     # Apply the transformation stored in tmp/MNI_to_t1_transform to the ROI atlas
-    roi_mask_path = os.path.join("tmp", "MNI_to_t1_transform", "roi_mask.nii.gz")
-    apply_transform_to_image(input_image=ATLAS_ROI, transform=os.path.join('tmp', 'MNI_to_t1_transform', 'TransformParameters.0.for_labels.txt'), output_image=roi_mask_path)
+    roi_mask_output_dir = os.path.join("tmp", "MNI_to_t1_transform")
+    apply_transform_to_image(input_image=ATLAS_ROI, transform=os.path.join('tmp', 'MNI_to_t1_transform', 'TransformParameters.0.for_labels.txt'), output_dir=roi_mask_output_dir)
     print("\nROI mask transformed to native space.\n")
 
     # Preprocess and use the transformed atlas ROI to crop T1 and T2 images
+    roi_mask_path = os.path.join("tmp", "MNI_to_t1_transform", "roi_mask.nii.gz")
     crop_and_preprocess_images_method_II(t1_ss_path, t2_ss_path, roi_mask_path)
     print("\nImages preprocessed and cropped using the transformed ROI mask.\n")
 
