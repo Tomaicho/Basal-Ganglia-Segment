c39619bbcbe6c1446db1615f40d2ec9161301e45
Tomaicho <tomas.a.lima@gmail.com>
2025-01-30 15:12:32 +0100

    install_model

--- install_model.py
+++ install_model.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 from nnunetv2.model_sharing.model_import import install_model_from_zip_file
 
 pwd = os.path.dirname(os.path.abspath(__file__))
@@ -10,9 +11,12 @@ os.environ['nnUNet_preprocessed'] = os.path.join(pwd, "nnunet", "preprocessed")
 os.environ['nnUNet_results'] = os.path.join(pwd, "nnunet", "models")
 
 # Export the paths of the nnuNet folders in terminal
-os.system(f'export nnUNet_raw={os.path.join(pwd, "nnunet", "raw")}')
-os.system(f'export nnUNet_preprocessed={os.path.join(pwd, "nnunet", "preprocessed")}')
-os.system(f'export nnUNet_results={os.path.join(pwd, "nnunet", "models")}')
+cmd = f'export nnUNet_raw={os.path.join(pwd, "nnunet", "raw")}'
+subprocess.run(cmd, shell=False)
+cmd = f'export nnUNet_preprocessed={os.path.join(pwd, "nnunet", "preprocessed")}'
+subprocess.run(cmd, shell=False)
+cmd = f'export nnUNet_results={os.path.join(pwd, "nnunet", "models")}'
+subprocess.run(cmd, shell=False)
 
 if len(sys.argv) != 2:
     print("Usage: python install_model.py /path/to/model.zip")
