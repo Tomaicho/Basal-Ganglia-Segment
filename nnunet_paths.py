967b2f95ad32bddf9faa266877f75c7c7a19fd0c
Tomaicho <tomas.a.lima@gmail.com>
2025-01-30 16:14:20 +0100

    nnunet_paths

--- nnunet_paths.py
+++ nnunet_paths.py
@@ -3,6 +3,6 @@ import subprocess
 
 pwd = os.path.dirname(os.path.abspath(__file__))
 
-subprocess.run(f'export nnUNet_raw="{os.path.join(pwd, "nnunet")}"', shell=True)
-subprocess.run(f'export nnUNet_preprocessed="{os.path.join(pwd, "nnunet")}"', shell=True)
-subprocess.run(f'export nnUNet_results="{os.path.join(pwd, "nnunet", "models")}"', shell=True)
+subprocess.run(f'echo export nnUNet_raw="{os.path.join(pwd, "nnunet")}"', shell=True)
+subprocess.run(f'echo export nnUNet_preprocessed="{os.path.join(pwd, "nnunet")}"', shell=True)
+subprocess.run(f'echo export nnUNet_results="{os.path.join(pwd, "nnunet", "models")}"', shell=True)
