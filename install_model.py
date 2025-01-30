4d9af641dd67d132e0e238f66fb199330fbb962f
Tomaicho <tomas.a.lima@gmail.com>
2025-01-30 15:08:07 +0100

    install_model

--- install_model.py
+++ install_model.py
@@ -9,6 +9,11 @@ os.environ['nnUNet_raw'] = os.path.join(pwd, "nnunet", "raw")
 os.environ['nnUNet_preprocessed'] = os.path.join(pwd, "nnunet", "preprocessed")
 os.environ['nnUNet_results'] = os.path.join(pwd, "nnunet", "models")
 
+# Export the paths of the nnuNet folders in terminal
+os.system(f'export nnUNet_raw={os.path.join(pwd, "nnunet", "raw")}')
+os.system(f'export nnUNet_preprocessed={os.path.join(pwd, "nnunet", "preprocessed")}')
+os.system(f'export nnUNet_results={os.path.join(pwd, "nnunet", "models")}')
+
 if len(sys.argv) != 2:
     print("Usage: python install_model.py /path/to/model.zip")
     sys.exit(1)
