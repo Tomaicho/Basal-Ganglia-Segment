0f7577eb3caf17c44a6ceb2a7fc46085f298b578
Tomaicho <tomas.a.lima@gmail.com>
2025-02-05 10:55:08 +0100

    f

--- install_model.py
+++ install_model.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 from nnunetv2.model_sharing.model_import import install_model_from_zip_file
 
 pwd = os.path.dirname(os.path.abspath(__file__))
@@ -18,4 +19,11 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 zip_file_path = sys.argv[1]
-install_model_from_zip_file(zip_file_path)
+
+# Define environment variables
+env = os.environ.copy()
+env["nnUNet_raw"] = os.path.join(pwd, "nnunet", "raw")
+env["nnUNet_preprocessed"] = os.path.join(pwd, "nnunet", "preprocessed")
+env["nnUNet_results"] = os.path.join(pwd, "nnunet", "models")
+
+subprocess.run(["python", "-c", f"from nnunetv2.model_sharing.model_import import install_model_from_zip_file; install_model_from_zip_file('{zip_file_path}')"], env=env)
